68e9bb2f5731612acd6e0f49e521ef0a1afe6a7c
Skud <skud@infotrope.net>
2013-01-31 11:46:07 +1100

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pear::Application.config.secret_token = '5db24ede90fd41a8437964f8ac02dfd15876d096e2886e9efd8f7b9c2c8eb746e2e6c8742700a54d81a482190a40224147da4daee617ed50cb1c49e77603f960'
