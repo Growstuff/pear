3c7bac6388461b7942c539d9c3b81bb41dcb3255
Skud <skud@infotrope.net>
2013-02-09 23:29:32 +1100

    added 'available' field to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130207233529) do
+ActiveRecord::Schema.define(:version => 20130209122523) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20130207233529) do
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
+    t.boolean  "available"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
