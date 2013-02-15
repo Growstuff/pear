510fd1b8767e1226d07ffe66c7d75fbc230e4671
Miles Gould <miles@assyrian.org.uk>
2013-02-15 23:15:37 +0000

    Use GitHub username rather than Real Name (TM).
    Everyone has a username, but many (inc me) don't have a Real Name set.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   def self.find_for_github_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     unless user
-      user = User.create(name:auth.extra.raw_info.name,
+      user = User.create(name:auth.extra.raw_info.login,
                           provider:auth.provider,
                           uid:auth.uid,
                           email:auth.info.email,
