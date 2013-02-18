8ec4cede0009a2cbb7bc10037ce42dc054c38a1d
Skud <skud@infotrope.net>
2013-02-19 09:39:20 +1100

    Improved front page table of available coders
    - split UTC offset into its own column (now DST aware)
    - sort coders by UTC offset

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,5 +15,36 @@ describe User do
       @user.update_attributes(:available => true)
       @user.available.should be true
     end
+
   end
+
+  context 'tz_order' do
+    before(:each) do
+      @london = FactoryGirl.create(:london_user)
+      @sydney = FactoryGirl.create(:sydney_user)
+      @la     = FactoryGirl.create(:la_user)
+    end
+
+    it 'can make timezones' do
+      ActiveSupport::TimeZone.new(@london.time_zone).should_not be_nil
+      ActiveSupport::TimeZone.new(@sydney.time_zone).should_not be_nil
+      ActiveSupport::TimeZone.new(@la.time_zone).should_not be_nil
+    end
+
+    it 'gets a list' do
+      User.tz_order.length.should eq 3
+    end
+
+    it 'sorts correctly' do
+      User.tz_order.first.should eq @la
+      User.tz_order.last.should  eq @sydney
+    end
+
+    it "doesn't die on a blank timezone" do
+      @new = FactoryGirl.create(:available_no_tz_user)
+      User.tz_order.length.should eq 4
+    end
+
+  end
+
 end
